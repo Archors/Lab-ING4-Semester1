0ecac2b6368ad250e1428793e7cafcf16beadd25
Archors <combeantoine78@gmail.com>
2020-10-06 23:25:37 +0200

    Lab 4 BD

---
+++ DB/Lab4/src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+import model.DataAccess;
+
+public class Test {
+
+  public static void main(String[] args) throws Exception {
+	  String url = "jdbc:mysql://localhost:3306/company";
+	  String username = "root";
+	  String pwd = "";
+	  DataAccess data = new DataAccess(url, username,pwd );	 
+	  data.close();
+  }
+}
